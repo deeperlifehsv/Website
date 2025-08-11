c041c49dce7322e7144adf1c1c7547cb3c4d77a2
John Adeyemo <ajohnofficial2020@gmail.com>
2025-08-11 04:54:52 -0500

    Fix fo build error

--- apps/web/src/lib/sanity.ts
+++ apps/web/src/lib/sanity.ts
@@ -5,9 +5,9 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANIT
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET || 'production'
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || process.env.SANITY_API_VERSION || '2024-01-01'
 
-if (!projectId) {
-  throw new Error('Missing Sanity Project ID')
-}
+// if (!projectId) {
+//   throw new Error('Missing Sanity Project ID')
+// }
 
 export const client = createClient({
   projectId,
