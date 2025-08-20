390cd66819142c9b4175491fa1c23321e777e3d3
John Adeyemo <ajohnofficial2020@gmail.com>
2025-08-20 15:07:49 -0500

    Update to next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -17,9 +17,9 @@ const nextConfig = {
     serverActions: true,
   },
   env: {
-    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
-    SANITY_DATASET: process.env.SANITY_DATASET,
-    SANITY_API_VERSION: process.env.SANITY_API_VERSION,
+  NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+  NEXT_PUBLIC_SANITY_DATASET: process.env.SANITY_DATASET,
+  NEXT_PUBLIC_SANITY_API_VERSION: process.env.SANITY_API_VERSION,
   },
 }
 
